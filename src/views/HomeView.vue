09b313e249d5e1b18f94cb0d68a7ff785b04b3cf
Vladislav Kozlovsky <vkozlovsky@atypicaldigital.com>
2022-10-13 01:28:45 +0300

    add slider

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,55 @@
 <template>
-  <div class="md-body">
-    <div class="card__wrapper" v-for="item in items" :key="item.id">
+  <carousel :breakPoints="breakPoints" :settings="settings">
+    <slide class="card__wrapper" v-for="item in items" :key="item.id">
       <card-component :title="item.title" :level="`${item.level} lvl`" :urlImg="item.img" :link="`/${item.alias}`">
         <template v-slot:body>{{ item.description }}</template>
         <template v-slot:footer>
           <div class="card-stats">
             <div v-for="(stat, index) in item.info" :key="index" class="one-third">
-              <div class="stat-value">{{stat.value}}</div>
-              <div class="stat">{{stat.title}}</div>
+              <div class="stat-value">{{ stat.value }}</div>
+              <div class="stat">{{ stat.title }}</div>
             </div>
           </div>
         </template>
       </card-component>
-    </div>
-  </div>
+    </slide>
+
+    <template #addons>
+      <navigation/>
+    </template>
+  </carousel>
 </template>
 
 <script>
 import CardComponent from '@/components/UI/CardComponent'
 import items from '../seeders/items'
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Navigation } from 'vue3-carousel'
 
 export default {
   name: 'HomeView',
-  components: { CardComponent },
+  components: {
+    CardComponent,
+    Carousel,
+    Slide,
+    Navigation
+  },
   data () {
     return {
-      items: items
+      items: items,
+      settings: {
+        itemsToShow: 2,
+        snapAlign: 'center',
+        wrapAround: true
+      },
+      breakPoints: {
+        300: {
+          itemsToShow: 1
+        },
+        700: {
+          itemsToShow: 2
+        }
+      }
     }
   }
 }
